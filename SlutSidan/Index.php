a5a1b8dca0f010ebac365c73da7b37d441dc8489
VictorDrott <32635979+VictorDrott@users.noreply.github.com>
2018-04-12 09:38:27 +0200

    k

---
+++ SlutSidan/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Maincss.css">
+    <title></title>
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Register</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
+  </body>
+</html>
