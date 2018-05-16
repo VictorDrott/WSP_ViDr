36643da64c7625cad7d2f30135786105790a9197
VictorDrott <32635979+VictorDrott@users.noreply.github.com>
2018-05-16 21:01:13 +0200

    hh

--- SlutSidan/includes/header.php
+++ SlutSidan/includes/header.php
@@ -4,10 +4,12 @@
       if(isset($_SESSION['mail'])){
         echo '<li class="welcome">
           Welcome ' . $_SESSION["mail"] . '
+        </li><li class="welcome">
+          Welcome ' . $_SESSION["mail"] . '
         </li>
         <li>
           <form action="php/loggout.php" method="POST">
-            <input type="submit" name="submit" value="loggout">
+            <button type="submit" name="submit" value="loggout"></button>
           </form>
         </li>';
       }else{
